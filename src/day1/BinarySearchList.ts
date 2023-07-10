5df12136fb78eef3f5dccb590b29f0763a4ea253
Felipe <felipemouradev@gmail.com>
2023-07-10 15:25:14 -0500

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length;
+    do {
+	let middle = Math.floor(low + (high - low) / 2);
+	if (haystack[middle] === needle) {
+	    return true;
+	}
+	else if (haystack[middle] < needle) {
+	    low = middle + 1;
+	}
+	else {
+	    high = middle;
+	}
+    } while (low < high);
+    return false;
 }
