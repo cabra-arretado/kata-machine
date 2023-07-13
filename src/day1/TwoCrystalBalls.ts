93b8e73495a48ede8ddfb49179b3b85007ad691c
Felipe <felipemouradev@gmail.com>
2023-07-12 20:58:23 -0500

    two balls sollution

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  // The big O of this is supposed to be O(sqrt n)
+  let breaks_sqrt = Math.floor(Math.sqrt(breaks.length));
+  let step = 0;
 
+  // find where the first balls break
+  for (; step < breaks.length; step += breaks_sqrt) {
+    if (breaks[step]) {
+      break;
+    }
+  }
+
+  // go back last step before it brokr
+  step -= breaks_sqrt;
+
+  // let's break that second ball babe!
+  for (let i = step; i < step + breaks_sqrt; i++) {
+    if (breaks[i]) {
+      return i;
+    }
+  }
+  return -1
 }
