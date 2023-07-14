da0288d97b7216dcd35f5aededf9da788b5fe3fe
Felipe <felipemouradev@gmail.com>
2023-07-14 13:22:45 -0500

    implement stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,46 @@
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  public head?: Node<T>;
+  public tail?: Node<T>;
 
-    
+  constructor() {
+    this.length = 0;
+    this.head = this.tail = undefined;
+  }
 
-    constructor() {
+  push(item: T): void {
+    let node = new Node(item)
+    if (!this.tail) {
+      this.head = this.tail = node
+    } else {
+      node.previous = this.tail
+      this.tail = node
     }
+    this.length++;
+  }
+  pop(): T | undefined {
+    if (!this.tail) {
+      return undefined
+    }
+    let tmp = this.tail
+    this.tail = this.tail.previous
+    this.length--;
+    return tmp.value
 
-    push(item: T): void {
+  }
+  peek(): T | undefined {
+    if (this.tail) {
+      return this.tail.value;
+    }
+    return undefined;
+  }
+}
 
+class Node<T> {
+  value: T;
+  previous?: Node<T>;
+  constructor(value: T) {
+    this.value = value
+  }
 }
-    pop(): T | undefined {
 
-}
-    peek(): T | undefined {
-
-}
-}
