55a463a00bad13218201631b3bdc53b60160a715
Felipe <felipemouradev@gmail.com>
2023-07-14 13:00:45 -0500

    implement queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,50 @@
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  public head?: Node<T>;
+  public tail?: Node<T>;
 
-    
+  constructor() {
+    this.length = 0;
+    this.head = this.tail = this.tail = undefined;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    if (!this.tail) {
+      this.head = this.tail = new Node(item);
+    } else {
+      this.tail.next = new Node(item)
+      this.tail = this.tail.next
     }
+    this.length++;
+  }
+  deque(): T | undefined {
+    if (this.head) {
+      let tmp = this.head.value
+      this.head = this.head.next
+      this.length--;
 
-    enqueue(item: T): void {
+      // if our list has a 1 length tail = head
+      if (this.length === 0) {
+        this.tail = this.head
+      }
 
+      return tmp;
+    }
+    return undefined;
+  }
+  peek(): T | undefined {
+    if (this.head) {
+      return this.head.value;
+    }
+    return undefined;
+  }
 }
-    deque(): T | undefined {
 
+class Node<T> {
+  value: T;
+  next?: Node<T>;
+  constructor(value: T) {
+    this.value = value;
+  }
 }
-    peek(): T | undefined {
 
-}
-}
