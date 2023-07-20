259e610f6e629f4d5a67b65f9b1e7f23cd0c64fe
Felipe <felipemouradev@gmail.com>
2023-07-20 09:45:04 -0500

    fix array list

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -3,9 +3,9 @@ export default class ArrayList<T> {
   public capacity: number;
   public array: Array<T>;
 
-  constructor() {
+  constructor(capacity: number) {
     this.length = 0;
-    this.capacity = 10;
+    this.capacity = capacity;
     this.array = new Array(this.capacity);
   }
 
@@ -18,19 +18,20 @@ export default class ArrayList<T> {
         newArray[i + 1] = this.array[i]
       }
       this.array = newArray
-      length++
+      this.length++
       return;
     } else {
       for (let i = this.length; i >= 0; i--) {
         this.array[i] = this.array[i - 1]
       }
       this.array[0] = item;
-      length++
+      this.length++
     }
 
   }
   insertAt(item: T, idx: number): void {
     this.array[idx] = item;
+    this.length++
   }
   append(item: T): void {
     if (this.length === this.capacity) {
@@ -40,13 +41,13 @@ export default class ArrayList<T> {
         newArray[i] = this.array[i]
       }
       this.array = newArray
-      length++
+      this.length++
       this.array[this.length] = item;
       return;
     }
     else {
       this.array[this.length] = item;
-      length++
+      this.length++
     }
   }
   remove(item: T): T | undefined {
@@ -56,7 +57,7 @@ export default class ArrayList<T> {
         for (let j = i; j < this.length; j++) {
           this.array[j] = this.array[j + 1]
         }
-        length--
+        this.length--
         return removed;
       }
     }
@@ -70,7 +71,7 @@ export default class ArrayList<T> {
     for (let i = idx; i < this.length; i++) {
       this.array[i] = this.array[i + 1]
     }
-    length--
+    this.length--
     return removed;
   }
 }
